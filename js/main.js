80adff99f48e80c0a5ef7c405d13d21af089feb5
fahim25 <fahimmuntasir201@gmail.com>
2020-08-17 18:02:53 +0600

    added magnificPopup lib

--- js/main.js
+++ js/main.js
@@ -6,3 +6,18 @@
 $(function () {
     new WOW().init();
 });
+
+/*
+-----------------------------
+|   | Pop Up
+-----------------------------
+*/
+$(function () {
+    $("#work").magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        gallery:{
+            enabled: true
+        }
+    });
+});
