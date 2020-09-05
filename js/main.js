83bdb85ff2a373e080d3e0bfc4f32da19a7d6a8c
fahim25 <fahimmuntasir201@gmail.com>
2020-09-06 02:27:03 +0600

    added jquery for backtotop and smooth scroll

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@
 -----------------------------
 */
 $(function () {
-    new WOW().init();
+	new WOW().init();
 });
 
 /*
@@ -13,13 +13,13 @@ $(function () {
 -----------------------------
 */
 $(function () {
-    $("#work").magnificPopup({
-        delegate: 'a',
-        type: 'image',
-        gallery: {
-            enabled: true
-        }
-    });
+	$("#work").magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		gallery: {
+			enabled: true
+		}
+	});
 });
 /*
 -----------------------------
@@ -28,13 +28,13 @@ $(function () {
 */
 
 $(function () {
-    $("#team-members").owlCarousel({
-        items: 3,
-        autoplay: true,
-        smartSpeed: 700,
-        loop: true,
-        autoplayHoverPause: true,
-    });
+	$("#team-members").owlCarousel({
+		items: 3,
+		autoplay: true,
+		smartSpeed: 700,
+		loop: true,
+		autoplayHoverPause: true,
+	});
 });
 
 /*
@@ -43,13 +43,13 @@ $(function () {
 -----------------------------
 */
 $(function () {
-    $("#customer-testimonial").owlCarousel({
-        items: 1,
-        autoplay: true,
-        smartSpeed: 700,
-        loop: true,
-        autoplayHoverPause: true,
-    });
+	$("#customer-testimonial").owlCarousel({
+		items: 1,
+		autoplay: true,
+		smartSpeed: 700,
+		loop: true,
+		autoplayHoverPause: true,
+	});
 });
 /*
 -----------------------------
@@ -57,10 +57,10 @@ $(function () {
 -----------------------------
 */
 $(function () {
-    $('.counter').counterUp({
-        delay: 30,
-        time: 2000
-    });
+	$('.counter').counterUp({
+		delay: 30,
+		time: 2000
+	});
 });
 
 /*
@@ -70,11 +70,42 @@ $(function () {
 */
 
 $(function () {
-    $("#clients-list").owlCarousel({
-        items: 4,
-        autoplay: true,
-        smartSpeed: 700,
-        loop: true,
-        autoplayHoverPause: true,
-    });
+	$("#clients-list").owlCarousel({
+		items: 4,
+		autoplay: true,
+		smartSpeed: 700,
+		loop: true,
+		autoplayHoverPause: true,
+	});
+});
+/*
+-----------------------------
+|   | Navigation
+-----------------------------
+*/
+
+$(function () {
+	$(window).scroll(function () {
+		if ($(this).scrollTop() < 50) {
+			//$('nav').removeClass('vesco-top-nav');
+			$('#back-to-top').fadeOut();
+		} else {
+			//$('nav').addClass('vesco-top-nav');
+
+			$('#back-to-top').fadeIn();
+		}
+	});
+});
+
+//Smooth Scroll
+$(function () {
+	$('a.smooth-scroll').click(function (event) {
+		event.preventDefault();
+
+		var section = $(this).attr('href');
+
+		$('html , body').animate({
+			scrollTop: $(section).offset().top - 64
+		}, 1000, "easeInOutExpo");
+	});
 });
